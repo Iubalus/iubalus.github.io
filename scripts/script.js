1fcf6852dbe42b85716a798f016247df49e1cfd5
Iubalus <>
2018-07-31 19:09:37 -0600

    Lazy load widget, embed twitter, and adjust styles

--- scripts/script.js
+++ scripts/script.js
@@ -8,16 +8,23 @@ $(function(){
             'Did you know GitHub lets you host a blog for free? Neato!',
             'Did you know the Developer Blog is a google doc? That might be a problem later...',
             'Don\'t expect too much from my page. It\'s all kinda taped together right now...',
-            'I think this new site look is pretty spiffy!'
+            'I think this new site look is pretty spiffy!',
+            '90% Widgets 10% probably other stuff I guess.',
+            'Follow me on Twitter over there to the right... I\'ll say stuff like \"Check out my site\"...',
+            'Do you like the purple? I like the purple. Some people haven\'t been too keen on it.'
         ];
         var anchor = $("[data-quip-anchor]")
         function pickQuip(){
             anchor.html(quips[Math.floor(Math.random()*quips.length)]);
-            quips.push('Each quip click makes this quip more likely! Little joke of mine');
         }
         pickQuip();
-
         $("body").on("click", ".welcome", pickQuip);
+
+        //save soundcloud widget render cost until the music tab is requested
+        $("body").on("click", "[data-tab='music']:not(.loaded)", function(){
+            $("#music-tab-content").html('<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/477779040&color=%23dcd4d0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>')
+            $("[data-tab='music']").toggleClass("loaded", true);
+        })
 
         $("body").on("click", "[data-tab]", function(){
             var clicked = $(this);
