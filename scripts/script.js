0a7f1e1bcabaea1216fddcd6bff6883bd0c9cd98
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2018-12-04 21:59:38 -0700

    Added some console support

--- scripts/script.js
+++ scripts/script.js
@@ -15,14 +15,6 @@ $(function(){
             'If I\'ve written something questionable, feel free to tweet me about it: Typos ttoo.',
             'I don\'t have a subscriber count here, but I do have Google Analytics... It tells me I visit my site a lot...'
         ];
-        var musicLibrary = [
-            {
-                name: 'Rhodes Aesthetic',
-                date: '2018-10-07',
-                source: 'https://drive.google.com/uc?export=download&amp;id=1nfA46_IsDF4-xX--GHWiL19bnpooa4lg',
-                type: 'audio/wav'
-            }
-        ]
         var anchor = $("[data-quip-anchor]")
         function pickQuip(){
             anchor.html(quips[Math.floor(Math.random()*quips.length)]);
@@ -46,73 +38,4 @@ $(function(){
         });
 
         $('[data-tab-content="dev-blog"]').html($("#blog-content").remove())
-        console({
-            target:$("[data-console]"),
-            dataOut:$("[data-command-out]"),
-            dataIn:$("[data-command-in]")
-        })
-
-		function console(setup){
-		    var target = setup.target;
-		    var dataOut = setup.dataOut;
-		    var dataIn = setup.dataIn;
-
-		    target.append('<span contenteditable="true" data-console-focus class="offscreen"></span>');
-		    target.on("click", function(){
-                $("[data-console-focus]").focus();
-		    });
-
-		    $("[data-console-focus]").keydown(function(event){
-            		    var excluded = [16,17,18,20,9,37,38,39,40,112,113,114,115,116,117,118,119,120,121,122,123,33,34,35,36,144];
-            		    for(var i = 0; i < excluded.length; i++){
-            		        if(event.keyCode == excluded[i]){
-            		            return;
-            		        }
-            		    }
-            		    if(event.keyCode == 8){
-            		        dataIn.text(dataIn.text().slice(0, -1))
-            		    } else if(event.keyCode == 13){
-            		        var command = dataIn.text();
-            		        consolePrintln(command);
-            		        if(command.substring(0, 6) === "manual"){
-            		            consolePrintln("\"play\" is the only other supported operation");
-            		        }
-            		        if(command.substring(0, 4) === "play"){
-            		            var selection = musicLibrary[0];
-            		            consolePrint("loading " + selection.name + " " + selection.date + "...");
-            		            $("[data-console-execute-wrap]").html(audioTemplate(selection));
-            		            playAudio();
-            		        }
-            		        dataIn.empty();
-            		    } else {
-            		        dataIn.append(event.key);
-            		    }
-            		});
-
-		    async function playAudio(){
-		        try{
-		            var song = target.find("[data-console-audio-song]")[0];
-		            song.load();
-		            await song.play();
-		            consolePrintln(" playing...");
-		        } catch(err){
-		            console.log(err);
-		        }
-		    }
-
-		    function audioTemplate(opts){
-		        var source = opts.source;
-		        var type = opts.type;
-		        return "<audio controls='' data-console-audio-song src='" + source + "' type='" + type + "'></audio>"
-		    }
-
-		    function consolePrint(message){
-		        dataOut.append('<span>' + message + '<span>');
-		    }
-
-		    function consolePrintln(message){
-		        consolePrint(message + '<br>');
-		    }
-
-		}
 });
