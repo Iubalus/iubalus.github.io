b905761c4148992e5a8e1ec60a27a439f290870e
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2019-07-04 21:56:33 -0600

    Start converting website to VueJS

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,4 @@
 $(function(){
-        var quips = [
-            'Welcome to jubalrife.com!',
-            'I\'m sure I\'ll think of something to write about',
-            'Want to hear music I\'ve made? Check out the music tab',
-            'Gee, You found my site. Neat!',
-            'Got questions or comments? Tweet me. It\'s a ghost town over there',
-            'Did you know GitHub lets you host a blog for free? Neato!',
-            'I re-skinned and abandoned the google doc, but I need to make a thing to generate the html for me.',
-            'Don\'t expect too much from my page. It\'s all kinda taped together right now...',
-            'I think this new site look is pretty spiffy!',
-            '90% Widgets 10% probably other stuff I guess.',
-            'Follow me on Twitter... I\'ll say stuff like \"Check out my site\"...',
-            'This used to be purple, but I got tired of that so it\'s bluish now',
-            'If I\'ve written something questionable, feel free to tweet me about it: Typos ttoo.',
-            'I don\'t have a subscriber count here. I used to have google analytics, but I like a bit of mystery so I got rid of that.',
-            'I\'m currently using patched together JQuery, but I\'ll probably switch to VueJS soon'
-        ];
-        var anchor = $("[data-quip-anchor]")
-        function pickQuip(){
-            anchor.html(quips[Math.floor(Math.random()*quips.length)]);
-        }
-        pickQuip();
-        $("body").on("click", ".welcome", pickQuip);
-
         //save soundcloud widget render cost until the music tab is requested
         $("body").on("click", "[data-tab='music']:not(.loaded)", function(){
             $("#music-tab-content").html('<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/477779040&color=%23dcd4d0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>')
