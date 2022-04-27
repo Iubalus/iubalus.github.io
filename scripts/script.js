73c7b124ed138bce85add6d81063e150d3aaa5d1
Jubal Rife <2788025+Iubalus@users.noreply.github.com>
2022-04-26 21:08:21 -0600

    Reduce dependency on JQuery
    Re-write the tab mechanism in the primary script so that it uses native javascript instead of JQuery.

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,38 @@
-$(function(){
-        //save soundcloud widget render cost until the music tab is requested
-        $("body").on("click", "[data-tab='music']:not(.loaded)", function(){
-            $("#music-tab-content").html('<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/477779040&color=%23dcd4d0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>')
-            $("[data-tab='music']").toggleClass("loaded", true);
+(function () {
+    let state = {
+        musicOpened: false
+    };
+    document.getElementById("open-console").addEventListener("click", () => {
+        window.open('console.html', 'Console', 'width=837,height=528');
+    });
+    document.getElementById("open-music-tab").addEventListener("click", () => {
+        console.log("open tab clicked")
+        if (!state.musicOpened) {
+            state.musicOpened = true;
+            document.getElementById("music-tab-content").innerHTML = '<iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/477779040&color=%23dcd4d0&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>';
+            document.getElementById("open-music-tab").classList.add("loaded");
+        }
+    });
+    let findByAttribute = function (tag, attribute) {
+        return Array.from(document.getElementsByTagName(tag))
+            .filter(d => Array.from(d.attributes)
+                .filter(f => f.name == attribute).length > 0)
+    }
+    let tabs = findByAttribute("td", "data-tab");
+    let contents = findByAttribute("div", "data-tab-content");
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(t => t.classList.remove("active"));
+            tab.classList.add("active");
+            let activeTab = tab.attributes.getNamedItem("data-tab").value;
+            contents
+                .forEach(c => {
+                    if (c.attributes.getNamedItem("data-tab-content").value === activeTab) {
+                        c.classList.add("active");
+                    } else {
+                        c.classList.remove("active");
+                    }
+                });
         })
-
-        $("body").on("click", "[data-tab]", function(){
-            var clicked = $(this);
-            var target = clicked.parents("[data-tabs]");
-            target.find("[data-tab]").toggleClass("active", false);
-            clicked.toggleClass("active", true);
-            target.find("[data-tab-content]").toggleClass("active", false);
-            target.find("[data-tab-content='"+ clicked.data("tab")+"']").toggleClass("active", true);
-        });
-
-        $("body").on("click", "[data-open-console]", function(){
-            window.open('console.html', 'Console', 'width=837,height=528');
-        });
-});
+    });
+})();
